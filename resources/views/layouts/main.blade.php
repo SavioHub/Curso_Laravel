d57d0434a5513e4f92628e9ac478528ceb0846e4
savioHub <saviosantossilva19@gmail.com>
2021-09-21 10:49:43 -0300

    iniciando css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/css/styles.css">
+        <link rel="stylesheet" href="/css/global.css">
+
         <script src="/js/index.js"></script>
 
         <title>@yield('title')</title>
@@ -19,31 +21,38 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     </head>
     <body>
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="collapse navbar-collapse" id="navbar">
-                    <a href="/" class="navbar-brand">
-                        <img src="/img/logo.ico" alt="logo do site">
-                    </a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Eventos</a>
-                        </li> <li class="nav-item">
-                            <a href="/events/create" class="nav-link">Criar Eventos</a>
-                        </li> <li class="nav-item">
-                            <a href="/" class="nav-link">Entrar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Cadastrar</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        <div id="home" class="content">
+            <header>
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <div id="navbar" class="collapse navbar-collapse">
+                        <a href="/" class="navbar-brand">
+                            <img src="/img/Agro_vendas.png" alt="logo do site">
+                        </a>
+    
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Eventos</a>
+                            </li> <li class="nav-item">
+                                <a href="/events/create" class="nav-link">Criar Eventos</a>
+                            </li> <li class="nav-item">
+                                <a href="/" class="nav-link">Entrar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Cadastrar</a>
+                            </li>
+                        </ul>
+    
+                    </div>
+                </nav>
+            </header>
+            
+            @yield('content')
+    
+            <footer>
+                <p> Agro Vendas &copy; 2022</p>
+            </footer>
+            
+        </div>
         
-        @yield('content')
-        <footer>
-            <p> Carol Calcados &copy; 2021</p>
-        </footer>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,34 @@
 @extends('layouts.main')
 
-@section('title', 'Carol Calcados')
+@section('title', 'Agro vendas')
 
 @section('content')
 
-<div id="seach-container" class="col-md-12">
-    <h2>Busque o evento</h2>
+<div id="search-container" class="col-md-12">
+    <h2>Busque um produto</h2>
+
     <form action="">
+        <label class="sr-only" for="search">Busca por produtos</label>
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar">
+        <button>buscar</button>
     </form>
 </div>
 
 <div id="events-container" class="col-md-12">
-    <h3>Proximos Eventos</h3>
-    <p>Veja os eventos dos próximos dias</p>
+    <h3>Listas de produtos</h3>
+    <p class="subtitle">Veja a lista de protudos mais recentes</p>
 
     <div id="cards-container" class="row">
         @foreach ($events as $event)
 
             <div class="card col-md-3">
-                <img src="/img/event_placeholder.jpg" alt="{{$event->title}}">
+                <img src="/img/Agro_vendas.png" alt="{{$event->title}}">
                 
                 <div class="card-body">
                     <p class="card-date">13/09/2021</p>
                     <h5 class="card-title">{{$event->title}}</h5>
                     <p class="card-participants">X participantes</p>
-                    <a href="#" class="btn btn-primary">Saber nais</a>
+                    <a href="#" class="btn btn-primary" id="btn">Saber nais</a>
                 </div>
             </div>
 
